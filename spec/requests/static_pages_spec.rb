b659da8b8e3d9c925853d16b630ddcfbc309d00a
Tan <selenet@mbp15-selenet.telenav.com>
2013-07-10 13:35:24 -0700

    Initial User model creation

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -56,8 +56,8 @@ describe "StaticPages" do
     page.should have_selector 'title', text: full_title('Contact')
     click_link "Home"
     page.should have_selector 'title', text: full_title('')
-    #click_link "Sign up now!"
-    #page.should have_selector 'title', text: full_title('Sign up')
+    click_link "Sign up now!"
+    page.should have_selector 'title', text: full_title('Sign up')
     click_link "sample app"
     page.should have_selector 'title', text: full_title('')
     
