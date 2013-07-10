b225d8ee0fa3822307f4ec688071d3613e066199
Tan <selenet@mbp15-selenet.telenav.com>
2013-07-10 10:30:14 -0700

    Extra exercises for Part 3 static pages, .gitignore for bin, guardfile updates

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,7 +6,7 @@ describe "StaticPages" do
   let(:app_title) {'Ruby on Rails Tutorial Sample App'}
   
   describe "Home page" do
-    it "should have the content 'Sample App'" do
+    it "should have the h1 'Sample App'" do
       visit '/static_pages/home'
       page.should have_selector('h1', :text => 'Sample App')
     end
@@ -14,12 +14,19 @@ describe "StaticPages" do
     it "should have the title 'Home'" do
       visit '/static_pages/home'
       page.should have_selector('title',
-                                  :text => "Home#{title_divider}#{app_title}")
+                                  :text => app_title)
     end
+    
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title',
+                                      :text => 'Home |')
+    end
+    
   end
   
   describe "Help page" do
-    it "should have the content 'Help'" do
+    it "should have the h1 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('h1', :text => 'Help')
     end
@@ -32,7 +39,7 @@ describe "StaticPages" do
   end
   
   describe "About page" do
-    it "should have the content 'About Us'" do
+    it "should have the h1 'About Us'" do
       visit '/static_pages/about'
       page.should have_selector('h1', :text => 'About Us')
     end
