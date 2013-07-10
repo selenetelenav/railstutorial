f04edb0238bcefbfdd3a4898bc5b301b9a60f3b3
Tan <selenet@mbp15-selenet.telenav.com>
2013-07-10 14:45:54 -0700

    Added basic password validation and authentication Uses Rails built-in has_secure_password Does not use a salt! I put in a placeholder but haven't figured out how to use it.

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,9 @@
 #
 
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
+  has_secure_password
+  # How do I add the salt??
   
   before_save { |user| user.email = email.downcase }
   
@@ -20,4 +22,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, 
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true
+  
+  
 end
