5a6e6362e51696121b0d7bad544f5838837c6e44
Selene Tan <selenet@telenav.com>
2013-07-12 19:01:56 -0700

    Set up generation for remember-session token

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
   has_secure_password
   
   before_save { |user| user.email = email.downcase }
+  before_save :create_remember_token
   
   
   validates :name, presence: true, length: { maximum: 50 }
@@ -28,5 +29,8 @@ class User < ActiveRecord::Base
     self.errors.messages.delete(:password_digest)
   end
   
-  
+  private
+    def create_remember_token
+      self.remember_token = SecureRandom.urlsafe_base64
+  end
 end
