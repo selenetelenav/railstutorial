1adc2b21c7da3ce9be654125afd05ca765af2d2a
Selene Tan <selenet@telenav.com>
2013-07-17 15:12:35 -0700

    Cleaned up duplicated code in tests
    Added helpers and custom matchers to remove some duplication from the
    page tests

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -5,16 +5,14 @@ describe "UserPages" do
   
   describe "signup page" do
     before { visit signup_path }
-    it { should have_selector('h1', text: 'Sign up') }
-    it { should have_selector('title', text: full_title('Sign up')) }
+    it { should have_title_heading('Sign up') }
   end
   
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
     
-    it { should have_selector('h1', text: user.name) }
-    it { should have_selector('title', text: user.name) }
+    it { should have_title_heading(user.name) }
   end
   
   
@@ -30,7 +28,7 @@ describe "UserPages" do
       
       describe "after submission" do
         before { click_button submit }
-        it { should have_selector('title', text: 'Sign up') }
+        it { should have_title_heading('Sign up') }
         it { should have_content('error') }
         it { should have_selector('.alert-error') }
       end
@@ -52,7 +50,7 @@ describe "UserPages" do
         before { click_button submit }
         let(:user) { User.find_by_email('user@example.com') }
         
-        it { should have_selector('title', text: user.name) }
+        it { should have_title_heading(user.name) }
         it { should have_selector('div.alert.alert-success', text: 'Welcome') }
         it { should have_link('Sign out')}
       end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,1 +1,20 @@
 include ApplicationHelper
+
+def valid_signin(user)
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+end
+
+RSpec::Matchers.define :have_title_heading do |title|
+  match do |page|
+    expect(page).to have_selector('h1', text: title)
+    expect(page).to have_selector('title', text: title)
+  end
+end
+
+RSpec::Matchers.define :have_error_message do |message|
+  match do |page|
+    expect(page).to have_selector('div.alert.alert-error', text: message)
+  end
+end
