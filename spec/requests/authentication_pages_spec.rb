153cb3c7c65b2b4d66c464cd349c5b454555ba95
Selene Tan <selenet@telenav.com>
2013-07-22 17:47:11 -0700

    Added tests to make sure signed-in-only links don't show to non-signed-in users

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -19,6 +19,11 @@ describe "Authentication" do
       it { should have_selector('title', text: 'Sign in') }
       it { should have_error_message('Invalid') }
       
+      it { should_not have_link('Users',    href: users_path) }
+      it { should_not have_link('Profile') }
+      it { should_not have_link('Settings') }
+      it { should_not have_link('Sign out', href: signout_path) }
+      
       describe "after visiting another page" do
         before { click_link "Home" }
         it { should_not have_selector('div.alert.alert-error') }
